37e63ce9a140eb5134c8d687f411af0ec25b717b
Muhammad-Sharjeel143 <sharjeel.dar786786@gmail.com>
2021-10-08 11:39:33 +0600

    initial commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -65,3 +65,31 @@ $('#testi-caresoul').owlCarousel({
         }
     }
 })
+
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    rtl:false,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    smartSpeed:2000,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
